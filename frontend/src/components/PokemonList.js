b0ee155201b05340b3e44d81fdd1886031533c4b
Joe LaMothe <joseph.lamothe55@gmail.com>
2020-07-28 23:32:01 -0400

    Set a cutoff so runoff pokemon appearances dont appear as new pokemon and added in buttons to show different pokemon by generation

--- frontend/src/components/PokemonList.js
+++ frontend/src/components/PokemonList.js
@@ -5,20 +5,27 @@ import classes from './PokemonList.module.css';
 const PokemonList = (props) => {
 
     const renderPokemonSummary = (pokemon) => (
-		
-        <div className={classes.Divencase} onClick={() => props.onClickHandler(pokemon)}>
+		(pokemon.id >= props.idStart && pokemon.id <= props.idEnd) ?  
+        (<div className={classes.Divencase} onClick={() => props.onClickHandler(pokemon)}>
+            
 			{(pokemon.sprites[0]) ? (<img src={pokemon.sprites[0].front_default} />) : (<p>no image found</p>)}
             
             <p>#{pokemon.id}</p>
             <p className={classes.Name}>{pokemon.name}</p>
-        </div>
+        </div>) : (null)
     );
-
+    console.log(props.generationName)
     return (
+        
         <div className={classes.Encase}>
+
             {props.pokemon.map( renderPokemonSummary )}
-            <button onClick={props.getNext60Pokemon}>The next 60</button>
-        </div>
+            <div>
+            {(props.page <= 13 && props.pokemon.length-1 <= props.idEnd) ?
+                (<button onClick={props.getNext60Pokemon}>The next 60</button>) :
+                (null)}
+            </div>
+         </div>
     );
 };
 
--- frontend/src/containers/FeaturedPokedex.js
+++ frontend/src/containers/FeaturedPokedex.js
@@ -11,7 +11,11 @@ class FeaturedPokedex extends Component {
         this.state = {
             pokemon: [],
             selected: null,
-            page: 0
+            page: 0,
+            generationNumber: null,
+            generationName: "national",
+            idStart: 1,
+            idEnd: 999
         };
     }
 
@@ -41,15 +45,54 @@ class FeaturedPokedex extends Component {
             pokemon: this.state.pokemon.concat(morePokemon),
             page: this.state.page + 1
         })
-       
-       
     }
 
+    generationSelection = async (generationNumber, generationName, idStart, idEnd, page) => {
+
+        this.generationWipe()
+
+        const genSelect = await pokemonService.getAll(page);
+
+        this.setState({
+            pokemon: this.state.pokemon.concat(genSelect),
+            page: page + 1,
+            generationNumber: generationNumber,
+            generationName: generationName,
+            idStart: idStart,
+            idEnd: idEnd
+        })
+    }
+
+    generationWipe = async () => {
+        this.setState({
+            pokemon: [],
+            page: null
+        })
+    }
+    
     render() {
         return (
             <Aux>
+                <div>
+                    <h3>Generation selection</h3>
+                    <button onClick={() => this.generationSelection(1, "Kanto", 1, 151, 0)} >Gen 1</button>
+                    <button onClick={() => this.generationSelection(2, "Johto", 152, 251, 2)} >Gen 2</button>
+                    <button onClick={() => this.generationSelection(3, "Hoenn", 252, 386, 4)}>Gen 3</button>
+                    <button onClick={() => this.generationSelection(4, "Sinnoh", 387, 493, 6)}>Gen 4</button>
+                    <button onClick={() => this.generationSelection(5, "Unova", 494, 649, 8)}>Gen 5</button>
+                    <button onClick={() => this.generationSelection(6, "Kalos", 650, 721, 10)}>Gen 6</button>
+                    <button onClick={() => this.generationSelection(7, "Alola", 722, 809, 12)}>Gen 7</button>
+                </div>
                 {(this.state.pokemon.length > 0) ? 
-                    (<PokemonList pokemon={this.state.pokemon} onClickHandler={this.onClickHandler} getNext60Pokemon={this.getNext60Pokemon} />) :
+                    (<PokemonList 
+                        pokemon={this.state.pokemon} 
+                        onClickHandler={this.onClickHandler} 
+                        getNext60Pokemon={this.getNext60Pokemon} 
+                        page={this.state.page} 
+                        generationNumber={this.state.generationNumber}
+                        generationName={this.state.generationName}
+                        idStart={this.state.idStart}
+                        idEnd={this.state.idEnd} />) :
                     (<p>Loading pokemon...</p>)
                 }
                 {(this.state.selected) ?
